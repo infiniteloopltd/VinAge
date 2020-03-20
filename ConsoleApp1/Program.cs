73c344d1b75f060459b2afaa960db0217f4d97ac
fiach <fiach.reid@gmail.com>
2020-03-20 12:04:03 +0100

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ConsoleApp3ML.Model;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("AI Prediction of age of vin numbers");
+            Console.WriteLine("Training data: 10,000 rows spanish data");
+            // Add input data
+            var input = new ModelInput();
+            Console.WriteLine("Please enter a VIN:");
+            input.VIN = Console.ReadLine();
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine("The year is predicted to be: " + result.Prediction);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp3ML.Model;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+
+            input.VIN = "VF1BRCF0H37276882";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine(result.Prediction);
+        }
+    }
+}
